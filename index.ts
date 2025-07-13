47ffc6c39b59ab4c427867c1b4ac1b316d227535
t.yureneva <taisiya.yureneva@gmail.com>
2025-07-13 11:45:14 +0300

    HW #1 Another solution

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ let max: number = 49;
 
 const argv = process.argv;
 
-console.log("\n=== Loto imitator ===\n");
+console.log("=== Loto imitator ===\n");
 
 if (argv.length > 2 && argv.length < 5) {
     console.log("Invalid number of arguments. To use default values, run the script without arguments.\nTo use custom value specify 3 arguments: count, min value and max value.");
@@ -18,22 +18,30 @@ if (argv[3] && !Number.isNaN(Number(argv[3]))) min = Number(argv[3]);
 if (argv[4] && !Number.isNaN(Number(argv[4]))) max = Number(argv[4]);
 
 if (min >= max) {
-    console.log("min should be less than max");
+    console.log("Warning: min value should be less than max\n");
     process.exit(1);
 }
 if (count > max - min + 1) {
-    console.log("Cannot generate " + count + " unique numbers between " + min + " and " + max);
+    console.log("Warning: cannot select", count, "unique numbers between", min, "and", max, "\n");
     process.exit(1);
 }
 
 console.log(">>> Draw ", count, " of ", max - min + 1, "<<<\n");
 
-// A solution suggested by AI - it was too fast, I did not have a chance to reject it...
+// A solution suggested by AI - it was too fast and nice, I did not have a chance to reject it...
 let num: number[] = _.range(min, max );
 
 num = _.shuffle(num);
-/// Just to add more randomness - does not make too much sense though
-const rangeStart = _.random(0, max - count - 1);
-num = num.slice(rangeStart, rangeStart + count);
+num = num.slice(0, count);
 
 console.log(">>> Numbers:", num);
+
+// And below is my solution
+let otherNums: number[] = [];
+
+while (otherNums.length < count) {
+    const a = _.random(min, max);
+    if (!otherNums.includes(a)) otherNums.push(a);
+}
+
+console.log(">>> Numbers:", otherNums);
