495f44456d7964bb0ddad1e93d291cd734872351
t.yureneva <taisiya.yureneva@gmail.com>
2025-07-22 16:02:09 +0300

    await reading file content

--- index.ts
+++ index.ts
@@ -1,57 +1,8 @@
-import config from "config";
-import { readFile, writeFile } from "node:fs/promises"
+import {readFile} from "fs/promises";
 
-interface FilePaths {
-    initialFile: string;
-    codeFile: string;
-    commentFile: string;
+async function fileSize(path: string): Promise<number> {
+    const content = await readFile(path, {encoding: "binary"});
+    return content.length;
 }
 
-function getPaths(): FilePaths {
-    return {
-        initialFile: config.get("files.initialFile"),
-        codeFile: config.get("files.codeFile"),
-        commentFile: config.get("files.commentsFile")
-    };
-}
-
-async function getText(file: string) : Promise<string> {
-    return await readFile(file, {encoding: "utf-8"});
-}
-
-function parseText(text: string) {
-    const lines = text.split('\n');
-    const comments: string[] = [];
-    const code: string[] = [];
-
-    for (const line of lines) {
-        const commentStart = line.indexOf('//');
-
-        if (commentStart < 0) {
-            code.push(line);
-        } else if (commentStart === 0) {
-            comments.push(line);
-        } else {
-            code.push(line.slice(0, commentStart));
-            comments.push(line.slice(commentStart));
-        }
-    }
-
-    const codeString = code.join('\n');
-    const commentsString = comments.join('\n');
-
-    return { commentsString, codeString };
-}
-
-async function writeToFile(file: string, text: string) {
-    await writeFile(file, text);
-}
-
-const paths = getPaths();
-
-const text = await getText(paths.initialFile);
-
-const parsed = parseText(text);
-
-await writeToFile(paths.codeFile, parsed.codeString).catch((err) => console.log(err));
-await writeToFile(paths.commentFile, parsed.commentsString).catch((err) => console.log(err));
+fileSize("large_file").then(len => console.log(len));
