6a32312bc36121b16672edcaf0f1286b381b3a2a
t.yureneva <taisiya.yureneva@gmail.com>
2025-08-03 18:14:55 +0300

    HW #7 Fixed. express-rate-limit used

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express";
 import { requestTime } from "../middleware/requestTime.ts";
-import { limitRequests, TooManyRequestsError } from "../middleware/limitRequests.ts";
+import rateLimit from "../../node_modules/express-rate-limit/dist/index.cjs";
 
 const port = 3501;
 
@@ -8,21 +8,14 @@ const app = express();
 app.listen(port, () => console.log(`Server started on port ${port}`));
 app.use(express.json());
 
-app.post("/api/greet", requestTime, limitRequests, (req: Request & {requestTime: string, error: Error}, res) => {
-    res.statusCode = 200;
-    let result: string = "";
-    if (req.error) {
-        res.statusCode = req.error instanceof TooManyRequestsError ? 429 : 400;
-        result = req.error.message;
-    }
-    else {
-        result = JSON.stringify({"message": "Hello!", "requestedAt": req.requestTime});
-    }
+app.use(requestTime)
 
-    res.end(result);
+app.post("/api/greet", rateLimit({windowMs: 60 * 1000, max: 3}), (req: Request & {requestTime: string, error: Error}, res) => {
+    res.statusCode = 200;
+    res.json({"message": "Hello!", "requestedAt": req.requestTime});
 })
 
-app.get("/api/status", requestTime, (req: Request & {requestTime: string}, res) => {
+app.get("/api/status", (req: Request & {requestTime: string}, res) => {
     res.statusCode = 200;
-    res.end(JSON.stringify({"status": "Up and Running", "requestedAt": req.requestTime}));
+    res.json({"status": "Up and Running", "requestedAt": req.requestTime});
 })
